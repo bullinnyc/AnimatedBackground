b4102427eacd5e4848d195be986aeca89d3c7cdc
bullinnyc <i8u11@icloud.com>
2021-09-09 14:28:48 +0700

    Initial Commit

---
+++ AnimatedBackground/AnimatedBackgroundApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedBackgroundApp.swift
+//  AnimatedBackground
+//
+//  Created by Dmitry Kononchuk on 09.09.2021.
+//  Copyright © 2021 Dmitry Kononchuk. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedBackground/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AnimatedBackground
+//
+//  Created by Dmitry Kononchuk on 09.09.2021.
+//  Copyright © 2021 Dmitry Kononchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
